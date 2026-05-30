ee8414bacb0d6882bf1ef1babf71b65295b9d049
Rafael-Procopio <rafaeljapcunha@gmail.com>
2026-05-30 18:58:27 -0300

    feat: implement main manu and screen navigation

---
+++ ZePilintra/src/game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+typedef enum
+{
+    SCREEN_MENU,
+    SCREEN_GAME,
+    SCREEN_PAUSE
+} GameScreen;
+
+#endif
--- ZePilintra/src/main.c
+++ ZePilintra/src/main.c
@@ -1,35 +1,84 @@
-#include <raylib.h>
-
-#define SCREEN_WIDTH 1200
-#define SCREEN_HEIGHT 800
+#include "raylib.h"
+#include "game.h"
 
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Zé Pilintra");
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
+    InitWindow(screenWidth, screenHeight, "Ze Pilintra");
     SetTargetFPS(60);
+
+    // Desabilita o fechamento automático com ESC
+    SetExitKey(KEY_NULL);
+
+    GameScreen currentScreen = SCREEN_MENU;
 
     while (!WindowShouldClose())
     {
+        // UPDATE
+        switch (currentScreen)
+        {
+            case SCREEN_MENU:
+            {
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = SCREEN_GAME;
+                }
+
+                // ESC fecha o jogo quando estiver no menu
+                if (IsKeyPressed(KEY_ESCAPE))
+                {
+                    break;
+                }
+
+                break;
+            }
+
+            case SCREEN_GAME:
+            {
+                // ESC volta para o menu
+                if (IsKeyPressed(KEY_ESCAPE))
+                {
+                    currentScreen = SCREEN_MENU;
+                }
+
+                break;
+            }
+
+            case SCREEN_PAUSE:
+            {
+                break;
+            }
+        }
+
+        // DRAW
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText(
-            "Zé Pilintra",
-            SCREEN_WIDTH / 2 - 180,
-            50,
-            40,
-            BLACK
-        );
+        switch (currentScreen)
+        {
+            case SCREEN_MENU:
+            {
+                DrawText("ZE PILINTRA", 400, 180, 60, BLACK);
+                DrawText("ENTER - Iniciar", 450, 320, 30, DARKGRAY);
+                DrawText("Versao 0.1", 20, 680, 20, GRAY);
+                break;
+            }
 
-        DrawText(
-            "Primeira versao",
-            SCREEN_WIDTH / 2 - 100,
-            100,
-            20,
-            DARKGRAY
-        );
+            case SCREEN_GAME:
+            {
+                DrawText("FASE 1 - NÃO SEI AINDA", 420, 180, 50, BLACK);
+                break;
+            }
+
+            case SCREEN_PAUSE:
+            {
+                DrawText("PAUSADO", 500, 200, 50, BLACK);
+                break;
+            }
+        }
 
         EndDrawing();
     }
